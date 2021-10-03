976314f9c70fd797d1213a9bb95c6f4db820023e
Vihrogonov <vihrogonov@protonmail.com>
2021-10-03 20:26:37 +0100

    Create InfoSection Component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import Dropdown from './components/Dropdown';
 import GlobalStyle from './components/globalStyles';
 import Hero from './components/Hero';
+import InfoSection from './components/InfoSection';
 import Navbar from './components/Navbar';
 import { SliderData } from './data/SliderData';
+import { InfoData } from './data/InfoData';
+import { InfoDataTwo } from './data/InfoData';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,6 +21,8 @@ function App() {
       <Navbar toggle={toggle} />
       <Dropdown isOpen={isOpen} toggle={toggle} />
       <Hero slides={SliderData} />
+      <InfoSection {...InfoData} />
+      <InfoSection {...InfoDataTwo} />
     </>
   );
 }
---
+++ src/data/InfoData.js
@@ -0,0 +1,26 @@
+import ImageOne from '../images/house-5.jpg'
+import ImageTwo from '../images/house-10.jpg'
+
+export const InfoData = {
+  heading: 'Explore our beautiful homes',
+  paragraphOne:
+    'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati quasi voluptatum animi alias provident repellat cum totam expedita. Nostrum eos reprehenderit.',
+  paragraphTwo:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis explicabo ipsam qui enim.',
+  buttonLabel: 'View Homes',
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: 'Modern Designs',
+  paragraphOne:
+    'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati quasi voluptatum animi.',
+  paragraphTwo:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis explicabo.',
+  buttonLabel: 'View Homes',
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
